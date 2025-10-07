1ca16e06d431e1c846e98b466fbcb5abf899c8b6
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-10-07 17:25:14 -0600

    Delete homeowrk15.py

--- homeowrk15.py
+++
@@ -1,7 +0,0 @@
-try:
-    age = int(input("Enter your age: "))
-    if age < 18:
-        raise ValueError("Age must be at least 18.")
-    print("Access granted.")
-except ValueError as ve:
-    print("Access denied. You must be at least 18 years old.")
