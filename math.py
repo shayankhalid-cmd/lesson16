cc80e44fcbffbdf62814044a45186ecf32667eab
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-10-07 17:55:21 -0600

    Add files via upload

---
+++ math.py
@@ -0,0 +1,10 @@
+import math
+print("the floor and ciling value of 23.56 are:"+
+str(math.ceil(23.56))+ ","+ str(math.floor(23.56)))
+x = 10
+y = -15
+print("the value of x after copying the sign for y is"+
+str(math.copysign(x,y)))
+print("the absoulute value of -96 and 56 are:"+
+str(math.fabs(-96))+ ","+ str(math.fabs(56)))
+print("the GCD of 24 and 56 is:"+str(math.gcd(24,56)))
