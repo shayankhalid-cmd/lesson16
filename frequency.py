ca83a898e3d81bbaa2a18a8ac5705ec585e8b4df
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-10-13 17:59:37 -0600

    Add files via upload

---
+++ frequency.py
@@ -0,0 +1,8 @@
+test_dict = {'condingal': 2, 'is' : 2, ' best': 2, ' for' : 2,'coding' : 1}
+print("the original dictonairy:" + str(test_dict))
+k = 2
+res = 0
+for key in test_dict:
+    if test_dict[key] == k:
+        res = res+1
+print("frequency of k is" + str(res))
