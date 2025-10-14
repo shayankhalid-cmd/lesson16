7463b33310a44af028bbb36ac613b63a68d57a3f
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-10-14 17:40:50 -0600

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,8 @@
+setx = {"green","blue"}
+sety = {"blue","yellow"}
+print("original set elements")
+print(setx)
+print(sety)
+print("\nintersection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
