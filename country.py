bf4c98d31ab8d1c9930bedb5b230d568597fe16d
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-10-14 18:03:53 -0600

    Add files via upload

---
+++ country.py
@@ -0,0 +1,5 @@
+country_code = {'India' : '0091','Austrailia' : '0025', 'Nepal' : '00977'} 
+print ("country code for India - ")
+print(country_code.get('India', 'Not found'))
+print("country code for japan-")
+print(country_code.get('japan','not found'))
