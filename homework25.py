2ec3cc52993262bcc59116c2081c577d3c356e56
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-10-23 17:41:41 -0600

    Update homework25.py

--- homework25.py
+++ homework25.py
@@ -3,8 +3,9 @@ class circle:
     def __init__(self, radius):
         self.radius = radius
     def calcuate_area(self):
-        area = pi * (radius*radius)
+        area = 3.14 * (radius*radius)
         print("area of circle with radius", radius, "is:", area)
 radius = int(input("enter radius of circle: "))
 obj = circle(radius)
+
 obj.calcuate_area()
