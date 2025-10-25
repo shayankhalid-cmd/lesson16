ee38ff3ac5809eb3211a25ea6f3cfe9751c2ea38
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-10-24 18:11:13 -0600

    Add files via upload

--- homework25.py
+++ homework25.py
@@ -1,11 +1,13 @@
 class circle:
-    pi = 3.14
     def __init__(self, radius):
         self.radius = radius
     def calcuate_area(self):
         area = 3.14 * (radius*radius)
         print("area of circle with radius", radius, "is:", area)
+    def calculate_perimeter(self):
+        perimeter = 2 * 3.14 * radius
+        print("perimeter of a circle is", perimeter)
 radius = int(input("enter radius of circle: "))
 obj = circle(radius)
-
 obj.calcuate_area()
+obj.calculate_perimeter()
